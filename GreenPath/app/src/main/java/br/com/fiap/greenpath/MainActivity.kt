fd4d337d420c9fcbcaa41e4ca9bf4ca4db883a23
Mateus <mateus.barboza.domingues@gmail.com>
2025-09-01 19:48:46 -0300

    feat: create repository

---
+++ GreenPath/app/src/main/java/br/com/fiap/greenpath/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.com.fiap.greenpath
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.greenpath.ui.theme.GreenPathTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
